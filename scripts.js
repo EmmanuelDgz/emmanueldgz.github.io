cdb6ab6f2dcc2bf4cb48214dd0c10894863b9823
EmmanuelDgz <32411320+EmmanuelDgz@users.noreply.github.com>
2018-10-08 01:27:51 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,30 @@
+var blueOn = document.querySelector('#blueOn');
+var blueOff = document.querySelector('#blueOff');
+var greenOn = document.querySelector('#greenOn');
+var greenOff = document.querySelector('#greenOff');
+var whiteOn = document.querySelector('#whiteOn');
+var whiteOff = document.querySelector('#whiteOff');
+
+blueOn.onclick = function() {
+  alert("hola");
+}
+
+blueOff.onclick = function() {
+  alert("hola");
+}
+
+greenOn.onclick = function() {
+  alert("hola");
+}
+
+greenOff.onclick = function() {
+  alert("hola");
+}
+
+whiteOn.onclick = function() {
+  alert("hola");
+}
+
+whiteOff.onclick = function() {
+  alert("hola");
+}
